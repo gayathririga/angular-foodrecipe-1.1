fa26559520593582834c44e2aa7985db0fa6d2a5
gayathririga <mailtogayathri05@gmail.com>
2020-02-27 12:57:24 +0530

    2 files modified

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Ingredient } from '../shared/ingredients.model';
+import { Ingredient } from '../sharedfolder/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
