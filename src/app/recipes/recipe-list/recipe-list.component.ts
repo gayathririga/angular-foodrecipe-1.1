a692a6a2ae9aa165fc66db5f967b71165633ab28
gayathririga <mailtogayathri05@gmail.com>
2020-03-02 11:39:46 +0530

    1 file modified

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,14 @@ import { Recipe } from "../recipe.model";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [new Recipe('Test R','Test D', 'https://www.cscassets.com/recipes/wide_cknew/wide_32.jpg')];
+  recipes: Recipe[] = [new Recipe('Test R','Test D', 'https://www.cscassets.com/recipes/wide_cknew/wide_32.jpg'),
+    new Recipe('Test R','Test D', 'https://www.cscassets.com/recipes/wide_cknew/wide_32.jpg'),
+    new Recipe('Test R','Test D', 'https://www.cscassets.com/recipes/wide_cknew/wide_32.jpg')
+  ];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    
+  }
 }
